12aa91d219314ed14ef5292476e3cf10658855ee
Jason Wong <jcwwong91@gmail.com>
2026-05-26 15:27:18 -0700

    Add: hand-of-straights - submission-3

---
+++ Data Structures & Algorithms/hand-of-straights/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+
+        handCount = int(len(hand) / groupSize)
+
+        frequency = dict()
+        for n in hand:
+            frequency[n] = frequency.get(n, 0) + 1
+
+        for i in range(handCount):
+            heap = list()
+            for k, v in frequency.items():
+                heapq.heappush(heap, k)
+            k = heapq.heappop(heap)
+            # print(k, v)
+            for j in range(groupSize):
+                idx = k + j
+                frequency[idx] = frequency.get(idx, 0) - 1
+                if frequency[idx] < 0:
+                    # print(f'Missing in hand {i} starting {v}, on {idx}')
+                    return False
+                elif frequency[idx] == 0:
+                    del(frequency[idx])
+
+        
+        return True
+        
